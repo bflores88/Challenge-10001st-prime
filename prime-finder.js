b4032e81cfc46fceb2c0937e55ca94fda22d9311
Brenda Flores <46147329+bflores88@users.noreply.github.com>
2019-04-17 09:30:09 -1000

    Update prime-finder.js

--- prime-finder.js
+++ prime-finder.js
@@ -1,33 +1,3 @@
 module.exports = function(n) {
   // do work here
-  let count = 0;
-  let start = 2;
-  let nthPrime = 0;
-
-  function testPrime(number) {
-    if (number === 1) {
-      return false;
-    } else if (number === 2) {
-      return true;
-    } else {
-      for (let x = 2; x < number; x++) {
-        if (number % x === 0) {
-          return false;
-        }
-      }
-      return true;
-    }
-  }
-
-  while (count !== n) {
-    if (testPrime(start) === true) {
-      nthPrime = start;
-      count++;
-      start++;
-    } else {
-      start++;
-    }
-  }
-
-  return nthPrime;
 };
